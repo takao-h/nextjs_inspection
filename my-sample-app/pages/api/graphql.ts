9f77cf284cb769af5ae07843e6052a5042543c26
takao-h <takao.hayashi@zozo.com>
2022-10-26 15:38:49 +0900

    Add sample

---
+++ my-sample-app/pages/api/graphql.ts
@@ -0,0 +1,30 @@
+import { ApolloServer } from "apollo-server-micro";
+import { typeDefs } from "../../graphql/schema";
+import { resolvers } from "../../graphql/resolver";
+import Cors from "micro-cors";
+import { MicroRequest } from "apollo-server-micro/dist/types";
+import { ServerResponse } from "http";
+
+const cors = Cors();
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+const startServer = apolloServer.start();
+
+export default cors(async (req: MicroRequest, res: ServerResponse) => {
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+  await startServer;
+
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
